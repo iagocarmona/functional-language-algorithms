0acfaaffdd931566a02ec82b3fbd577cb14f767f
Iago Carmona <iagoortegacarmona@gmail.com>
2023-11-13 16:39:05 -0300

    feat: adding readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # functional-language-algorithms
+
+---
+
+# Como executar?
+
+```bash
+ghci exercises.hs
+```
+
+### Vai abrir o terminal do `ghci`
+
+### Escreva `main` para mostrar as opções:
+
+```bash
+exec01 - Testar se um elemento é membro de uma lista
+exec02 - Calcular o tamanho de uma lista
+exec03 - Calcular a soma dos elementos de uma lista
+exec04 - Calcular o produto dos elementos de uma lista
+exec05 - Reversão de lista
+exec06 - Testar se duas listas são iguais
+exec07 - Concatenação de duas listas
+exec08 - Interseção de duas listas
+```
+
+### Para ver a execução de cada exercício, informe o label: Exemplo `exec01` ou `exec03`
+
+### Para sair do terminal do `ghci` digite `:q`
--- exercises.hs
+++ exercises.hs
@@ -8,67 +8,34 @@ main = do
   putStrLn "exec07 - Concatenação de duas listas"
   putStrLn "exec08 - Interseção de duas listas"
 
+-- ==================== GUSTAVO ========================
 exec01 = do
   putStrLn "Testar se um elemento é membro de uma lista"
 
-
+-- ==================== IAGO ========================
 exec02 = do 
   putStrLn "Calcular o tamanho de uma lista"
 
-exec03 :: IO ()
+-- ==================== GUSTAVO ========================
 exec03 = do 
   putStrLn "Calcular a soma dos elementos de uma lista"
 
-  putStrLn "Digite a lista (separada por espaços):"
-  input <- getLine
-  let lista = map read (words input) :: [Double]
-
-  putStr "Resultado: "
-  print (sum lista)
-
-exec04 :: IO ()
+-- ==================== IAGO ========================
 exec04 = do
   putStrLn "Calcular o produto dos elementos de uma lista"
 
-  putStrLn "Digite a lista (separada por espaços):"
-  input <- getLine
-  let lista = map read (words input) :: [Double]
-
-  putStr "Resultado: "
-  print (product lista)
-
+-- ==================== GUSTAVO ========================
 exec05 = do
   putStrLn "Reversão de lista"
 
-  putStrLn "Digite a lista"
-  lista <- getLine
-  
-  putStr "Resultado: "
-  print (reverse lista)
-
+-- ==================== IAGO ========================
 exec06 = do
   putStrLn "Testar se duas listas são iguais"
 
-  putStrLn "Digite a primeira lista"
-  lista1 <- getLine
-
-  putStrLn "Digite a segunda lista"
-  lista2 <- getLine
-  
-  putStr "Resultado: "
-  print (lista1 == lista2)
-
+-- ==================== GUSTAVO ========================
 exec07 = do
   putStrLn "Concatenação de duas listas"
 
-  putStrLn "Digite a primeira lista"
-  lista1 <- getLine
-
-  putStrLn "Digite a segunda lista"
-  lista2 <- getLine
-
-  putStr "Resultado: "
-  print (lista1 ++ lista2)
-
+-- ==================== IAGO ========================
 exec08 = do
   putStrLn "Interseção de duas listas"
