448995f3a1f4e451c02a2558af87c63f433dd5b7
Gustavo Martins <gtavomartins893@gmail.com>
2023-12-04 11:52:05 -0300

    implemented cutting stock with functional constructions

--- stock_cut/stock_cut.py
+++ stock_cut/stock_cut.py
@@ -1,16 +1,31 @@
+from functools import reduce
 import random
 import time
 
 
 def cutting_stock(orders, stock_length):
     
-    # Inicializando uma tabela para armazenar as soluções parciais
+    # Array para armazenar as soluções parciais
     dp = [float('inf')] * (stock_length + 1)
     dp[0] = 0   # Pedidos com comprimento zero são desconsiderados
 
     for order in orders:
         for length in range(order, stock_length + 1):
             dp[length] = min(dp[length], dp[length - order] + 1)
+
+    # O valor em dp[stock_length] agora contém o número mínimo de barras necessárias
+    return dp[stock_length]
+
+
+
+def cutting_stock_functional(orders, stock_length):
+    # Função auxiliar para atualizar a tabela dp
+    def update_dp(dp, order):
+        return [min(length, dp[length - order] + 1) if length - order >= 0 else dp[length] for length in range(stock_length + 1)]
+
+    # Inicialização da tabela dp
+    dp = reduce(update_dp, orders, [float('inf')] * (stock_length + 1))
+    dp[0] = 0
 
     # O valor em dp[stock_length] agora contém o número mínimo de barras necessárias
     return dp[stock_length]
@@ -33,20 +48,20 @@ if __name__ == "__main__":
 
 
     # calculando o tempo de execução da função com construções funcionais
-    # tempo_inicial = time.time()  # em segundos
-    # result = cutting_stock_fc(orders, stock_length)
-    # print(f"Número mínimo de barras de estoque necessárias: {result}")
-    # tempo_final = time.time()  # em segundos
+    tempo_inicial = time.time()  # em segundos
+    result = cutting_stock_functional(orders, stock_length)
+    print(f"Número mínimo de barras de estoque necessárias (fc): {result}")
+    tempo_final = time.time()  # em segundos
 
     # imprimindo o tempo de execução
-    # print("\nCom construções funcionais:", file=file)
-    # print("Tempo de execução: ", tempo_final - tempo_inicial, file=file)  # em segundos
+    print("\nCom construções funcionais:", file=file)
+    print("Tempo de execução: ", tempo_final - tempo_inicial, file=file)  # em segundos
 
 
     # calculando o tempo de execução da função sem construções funcionais
     tempo_inicial = time.time()  # em segundos
     result = cutting_stock(orders, stock_length)
-    # print(f"Número mínimo de barras de estoque necessárias: {result}")
+    print(f"Número mínimo de barras de estoque necessárias: {result}")
     tempo_final = time.time()  # em segundos
 
     # imprimindo o tempo de execução
